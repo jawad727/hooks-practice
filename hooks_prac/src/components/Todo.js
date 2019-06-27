6f494b06b2fae08781ad71de93f4bc33667ce42c
jawad727 <jawaddhussein@gmail.com>
2019-06-26 20:31:52 -0700

    Commented through old hooks code to prepare for next week

--- hooks_prac/src/components/Todo.js
+++ hooks_prac/src/components/Todo.js
@@ -1,17 +1,24 @@
 import React, {useState} from 'react';
 
 const Todo = props => {
-    const [todoName, setTodoName] = useState('')
-    const [todoList, setTodoList] = useState([])
+    // Here we are abstracting todoName and setTodoName from useState, now everytime we call todoName it will
+    // show the todoName and everytime we setTodoName it will change todoname
+    const [todoName, setTodoName] = useState('') 
+    const [todoList, setTodoList] = useState([]) //
 
+    // Here is a simple change handler, saying that when this is called, on every event (every letter typed)
+    // it will set the todoName
     const inputChangeHandler = event => {
         setTodoName(event.target.value)
     }
 
+    // Here is a simple add handler, saying that when it is invoked it will add the todoName to the list and then ser rodoName back to none (so someone else can type)
     const todoAddHandler = () => {
         setTodoList(todoList.concat(todoName))
+        setTodoName("")
     }
 
+    
     return(
         <React.Fragment>
             <input 
